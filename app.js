c9b0ec6a4d1e5a3fae30b23340fde2aba045748d
joelpdurham <joeldur@gmail.com>
2019-10-29 13:06:13 -0700

    finished filter

--- app.js
+++ app.js
@@ -3,10 +3,38 @@ import htmlToDOM from './utils/html-to-DOM.js';
 import images from './data/images.js';
 
 const ul = document.getElementById('cards');
+const horns = document.querySelector('.horns');
 
-images.forEach(item => {
-    let renderedImage = renderItem(item);
-    let domReadyIamge = htmlToDOM(renderedImage);
-    ul.appendChild(domReadyIamge);
+horns.addEventListener('change', () => {
+    const filter = horns.value;
+    let filteredHorns = null;
+
+    if (!filter) {
+        filteredHorns = images;
+    } else {
+        filteredHorns = images.filter(item => {
+            if (item.horns === filter) {
+                return item;
+            }
+        });
+    }
+    render(filteredHorns);
 });
+
+render(images);
+
+function render(imagesToRender) {
+    while (ul.lastElementChild) {
+        ul.lastElementChild.remove();
+    }
+
+    imagesToRender.forEach(item => {
+        let renderedImage = renderItem(item);
+        let domReadyIamge = htmlToDOM(renderedImage);
+        ul.appendChild(domReadyIamge);
+    });
+}
+
+
+
 
